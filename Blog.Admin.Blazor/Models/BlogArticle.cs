9dc4c81427ad1b06059a5dc25399589f2ac7e7e7
anjoy8 <3143422472@qq.com>
2020-05-25 16:57:23 +0800

    Initial commit

---
+++ Blog.Admin.Blazor/Models/BlogArticle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Admin.Blazor.Models
+{
+    public class BlogArticle
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int bID { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string bsubmitter { get; set; }
+
+        /// <summary>
+        /// 标题blog
+        /// </summary>
+        public string btitle { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public string bcategory { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string bcontent { get; set; }
+
+        /// <summary>
+        /// 访问量
+        /// </summary>
+        public int btraffic { get; set; }
+
+        /// <summary>
+        /// 评论数量
+        /// </summary>
+        public int bcommentNum { get; set; }
+
+        /// <summary> 
+        /// 修改时间
+        /// </summary>
+        public DateTime bUpdateTime { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public System.DateTime bCreateTime { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string bRemark { get; set; }
+
+        /// <summary>
+        /// 逻辑删除
+        /// </summary>
+        public bool? IsDeleted { get; set; }
+
+    }
+
+}
---
+++ Blog.Admin.Blazor/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace Blog.Admin.Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8081") });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
