5efeff5f965adf61fbc697b6151509fa3228b3c0
anjoy8 <3143422472@qq.com>
2020-06-03 16:59:51 +0800

    fix style

--- Blog.Core.Publish.Linux.sh
+++ Blog.Core.Publish.Linux.sh
@@ -1,6 +1,6 @@
 git pull;
 rm -rf .PublishFiles;
 dotnet build;
-dotnet publish -o /home/Blog.Admin.Blazor/Blog.Admin.Blazor/bin/Debug/netstandard2.1;
-cp -r /home/Blog.Admin.Blazor/Blog.Admin.Blazor/bin/Debug/netstandard2.1 .PublishFiles;
+dotnet publish -o /home/Blog.Admin.Blazor/Blog.Admin.Blazor/bin/Debug/netstandard2.1/wwwroot;
+cp -r /home/Blog.Admin.Blazor/Blog.Admin.Blazor/bin/Debug/netstandard2.1/wwwroot .PublishFiles;
 echo "Successfully!!!! ^ please see the file .PublishFiles";
