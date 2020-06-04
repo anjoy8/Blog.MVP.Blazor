3dffd1f6e5276c16198509a26629b264e08d1a1b
anjoy8 <3143422472@qq.com>
2020-06-04 13:15:14 +0800

    Update Blog.Admin.Blazor.Publish.Linux.sh

--- Blog.Admin.Blazor.Publish.Linux.sh
+++ Blog.Admin.Blazor.Publish.Linux.sh
@@ -1,6 +1,6 @@
 git pull;
 rm -rf .PublishFiles;
 dotnet build;
-dotnet publish -o /home/Blog.Admin.Blazor/Blog.Admin.Blazor/bin/Release/netstandard2.1/publish;
-cp -r /home/Blog.Admin.Blazor/Blog.Admin.Blazor/bin/Release/netstandard2.1/publish .PublishFiles;
+dotnet publish -o /home/Blog.MVP.Blazor/Blog.MVP.Blazor/bin/Release/netstandard2.1/publish;
+cp -r /home/Blog.MVP.Blazor/Blog.MVP.Blazor/bin/Release/netstandard2.1/publish .PublishFiles;
 echo "Successfully!!!! ^ please see the file .PublishFiles";
