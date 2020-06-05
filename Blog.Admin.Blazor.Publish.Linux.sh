e39a244852d1bb38cc57dade9e82121207e16664
anjoy8 <3143422472@qq.com>
2020-06-05 10:15:17 +0800

    add Blog.MVP.Blazor.SSR
    添加blazor 服务

--- Blog.Admin.Blazor.Publish.Linux.sh
+++ Blog.Admin.Blazor.Publish.Linux.sh
@@ -1,6 +1,7 @@
 git pull;
 rm -rf .PublishFiles;
 dotnet build;
-dotnet publish -o /home/Blog.MVP.Blazor/Blog.MVP.Blazor/bin/Release/netstandard2.1/publish;
-cp -r /home/Blog.MVP.Blazor/Blog.MVP.Blazor/bin/Release/netstandard2.1/publish .PublishFiles;
+cd Blog.MVP.Blazor.SSR
+dotnet publish -o /home/Blog.MVP.Blazor/Blog.MVP.Blazor.SSR/bin/Debug/netcoreapp3.1/publish;
+cp -r /home/Blog.MVP.Blazor/Blog.MVP.Blazor.SSR/bin/Debug/netcoreapp3.1/publish .PublishFiles;
 echo "Successfully!!!! ^ please see the file .PublishFiles";
---
+++ Blog.MVP.Blazor.SSR/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Blog.MVP.Blazor.SSR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .UseUrls("http://localhost:5050");
+                });
+    }
+}
