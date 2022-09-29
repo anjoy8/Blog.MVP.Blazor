449cd96cff60a1bfbeb44413d8d6e53b47cee697
ansonzhang <3143422472@qq.com>
2022-09-29 17:56:34 +0800

    Update Blog.Admin.Blazor.Publish.Linux.sh

--- Blog.Admin.Blazor.Publish.Linux.sh
+++ Blog.Admin.Blazor.Publish.Linux.sh
@@ -2,6 +2,6 @@ git pull;
 rm -rf .PublishFiles;
 dotnet build;
 cd Blog.MVP.Blazor.SSR
-dotnet publish -o /home/Blog.MVP.Blazor/Blog.MVP.Blazor.SSR/bin/Debug/netcoreapp3.1/publish;
-cp -r /home/Blog.MVP.Blazor/Blog.MVP.Blazor.SSR/bin/Debug/netcoreapp3.1/publish /home/Blog.MVP.Blazor/.PublishFiles;
+dotnet publish -o /home/Blog.MVP.Blazor/Blog.MVP.Blazor.SSR/bin/Debug/netcoreapp5.0/publish;
+cp -r /home/Blog.MVP.Blazor/Blog.MVP.Blazor.SSR/bin/Debug/netcoreapp5.0/publish /home/Blog.MVP.Blazor/.PublishFiles;
 echo "Successfully!!!! ^ please see the file .PublishFiles";
